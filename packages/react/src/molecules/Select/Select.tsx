2a7c74dbe234f0fd4f99a4467342769e29c0a53e
BauTancredi <bautistatancredi@hotmail.com>
2021-12-24 14:28:15 -0300

    Select component

---
+++ packages/react/src/molecules/Select/Select.tsx
@@ -0,0 +1,139 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import Text from "../../atoms/Text";
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+interface RenderOptionProps {
+  isSelected: boolean;
+  option: SelectOption;
+  getOptionRecommendedProps: (overrideProps: Object) => Object;
+}
+
+interface SelectProps {
+  onOptionSelected?: (option: SelectOption, optionIndex: number) => void;
+  options?: SelectOption[];
+  label?: string;
+  renderOption?: (props: RenderOptionProps) => React.ReactNode;
+}
+
+const Select: React.FC<SelectProps> = ({
+  options = [],
+  label = "Please select an option ...",
+  onOptionSelected: handler,
+  renderOption,
+}) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<null | number>(null);
+  const labelRef = useRef<HTMLButtonElement>(null);
+  const [overlayTop, setOverlayTop] = useState<number>(0);
+
+  const onOptionSelected = (option: SelectOption, optionIndex: number) => {
+    if (handler) handler(option, optionIndex);
+
+    setSelectedIndex(optionIndex);
+    setIsOpen(false);
+  };
+
+  const onLabelClicked = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setOverlayTop((labelRef.current?.offsetHeight || 0) + 10);
+  }, [labelRef.current?.offsetHeight]);
+
+  let selectedOption = null;
+
+  if (selectedIndex !== null) selectedOption = options[selectedIndex];
+  return (
+    <div className="dse-select">
+      <button
+        ref={labelRef}
+        className="dse-select__label"
+        onClick={() => onLabelClicked()}
+      >
+        <Text>{selectedIndex === null ? label : selectedOption?.label}</Text>
+        <svg
+          className={`dse-select__caret ${
+            isOpen ? "dse-select__caret--open" : "dse-select__caret--closed"
+          }`}
+          width="1rem"
+          height="1rem"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </button>
+      {isOpen ? (
+        <ul style={{ top: overlayTop }} className="dse-select__overlay">
+          {options.map((option, optionIndex) => {
+            const isSelected = selectedIndex === optionIndex;
+
+            const renderOptionProps = {
+              option,
+              isSelected,
+              getOptionRecommendedProps: (overrideProps = {}) => {
+                return {
+                  className: `dse-select__option ${
+                    isSelected ? "dse-select__option--selected" : ""
+                  }`,
+                  key: option.value,
+                  onClick: () => onOptionSelected(option, optionIndex),
+                  ...overrideProps,
+                };
+              },
+            };
+
+            if (renderOption) {
+              return renderOption(renderOptionProps);
+            }
+
+            return (
+              <li
+                onClick={() => onOptionSelected(option, optionIndex)}
+                key={option.value}
+                className={`dse-select__option ${
+                  isSelected ? "dse-select__option--selected" : ""
+                }`}
+              >
+                <Text>{option.label}</Text>
+                {isSelected ? (
+                  <svg
+                    width="1rem"
+                    height="1rem"
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                ) : null}
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+
+export default Select;
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Text, Margin } from "@ds.e/react";
+import { Select } from "@ds.e/react";
 
 import "@ds.e/scss/lib/Utilites.css";
 import "@ds.e/scss/lib/Text.css";
 import "@ds.e/scss/lib/Margin.css";
+import "@ds.e/scss/lib/Select.css";
 import "@ds.e/scss/lib/global.css";
 
+const options = [
+  {
+    value: "option-1",
+    label: "Option 1",
+  },
+  {
+    value: "option-2",
+    label: "Option 2",
+  },
+  {
+    value: "option-3",
+    label: "Option 3",
+  },
+];
+
 ReactDOM.render(
-  <div>
-    <Margin left>
-      <Text size="sm">This is some text</Text>
-    </Margin>
+  <div style={{ padding: "40px" }}>
+    <Select options={options} />
   </div>,
   document.querySelector("#root")
 );
